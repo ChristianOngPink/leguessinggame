67f28e8aa4991a118ec9bfec7412295c42ef2189
ChristianOngPink <krikri.ong135@gmail.com>
2026-03-24 00:19:46 -0700

    That forgetful commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function startGame() {
+    let randNum = Math.floor(Math.random () * 10) + 1;
+    let maxAttempts = 3;
+    
+    for (let i = 0; i < maxAttempts; i++) {
+        let userGuess = prompt("Try and guess a number betwen 1 and 10:");
+        userGuess = Number(userGuess);
+       
+        if (userGuess == randNum) {
+            alert("Congratulations! You guessed the number: " + randNum);
+            return;
+        }
+        else if (userGuess < randNum) {
+            alert("OOF!! Too Low Bro!!" + (maxAttempts - (i+1)) + " attempts Left.");
+        }
+        else {
+            alert("OOF!! Too High Guy!!" + (maxAttempts - (i+1)) + " attempts Left.");
+        }
+    }
+    alert("You Lose! The correct number was: " + randNum);
+}
